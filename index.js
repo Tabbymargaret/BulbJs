33e258f2f78a4d2c7a7b0be13d6b3ec7ed34ccdc
Margaret142 <tabbywange75@gmail.com>
2024-10-02 22:08:07 +0300

    Added files to github

---
+++ index.js
@@ -0,0 +1,8 @@
+function turnOff()
+{
+    document.getElementById('bulb').src='https://image.ibb.co/hoBxtm/light_bulb_off.png';
+}
+function turnOn()
+{
+    document.getElementById('bulb').src='https://i.ibb.co/qjSGTLb/light-bulb-on.png';
+}
